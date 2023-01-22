38e9025b7e38d54e1ae15227728a7f32646200a0
Josiah Adesola <59745353+Josiah-Adesola@users.noreply.github.com>
2023-01-22 05:03:41 +0100

    Add files via upload

---
+++ ChatGPT3-Clone-Gradio/app.py
@@ -0,0 +1,46 @@
+import os
+import gradio as gr
+import openai
+
+openai.api_key = "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"
+
+start_sequence = "\nAI:"
+restart_sequence = "\nHuman: "
+prompt = "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: ",
+
+def openai_create(prompt):
+    response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt = prompt,
+    temperature=0.9,
+    max_tokens=150,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0.6,
+    stop=[" Human:", " AI:"]
+    )
+    
+    return response.choices[0].text 
+
+
+def chatgpt_clone(input, history):
+    history = history or []
+    s = list(sum(history, ()))
+    s.append(input)
+    input = ''.join(s)
+    output = openai_create(input)
+    history.append((input, output))
+    return history, history
+
+block = gr.Blocks()
+
+with block:
+    gr.Markdown("""<h1><center>My ChatGPT with OpenAI & Gradio</center></h1>""")
+    chatbot = gr.Chatbot()
+    message = gr.Textbox(placeholder=prompt)
+    state = gr.State()
+    submit = gr.Button("SEND")
+    submit.click(chatgpt_clone, inputs=[message, state], outputs=[chatbot,state])
+    
+    
+block.launch(debug=True)
